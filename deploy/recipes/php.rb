1438a9093f0304dd2113cd00c6dbbcf4cfd85be0
StarClutch <shetter.blake@gmail.com>
2015-06-29 22:21:21 -0400

    Update php.rb

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -33,7 +33,7 @@ end
 
 node[:deploy].each do |application, deploy|
   brackets_dir = "#{deploy[:deploy_to]}/current/public/brackets"
-  execute "chmod 777 #{brackets_dir}" do
+  execute "chmod -R 777 #{brackets_dir}" do
   end
 end
 
