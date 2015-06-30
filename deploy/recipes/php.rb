fea94f3edb7111aca7766e928633af6f8507b349
StarClutch <shetter.blake@gmail.com>
2015-06-29 21:11:19 -0400

    Update php.rb

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -26,7 +26,7 @@ node[:deploy].each do |application, deploy|
 end
 
 node[:deploy].each do |application, deploy|
-  templates_dir = "#{deploy[:deploy_to]}/current/smarty"
+  templates_dir = "#{deploy[:deploy_to]}/current/smarty/templates_c"
   execute "chmod 777 #{templates_dir}" do
   end
 end
