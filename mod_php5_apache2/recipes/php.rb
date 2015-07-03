5f11f66107f4d0a9140e9f33e83ad522afb42c49
Tomasz Musialek <tomasz.musialek@gmail.com>
2015-07-04 00:01:35 +0200

    a

--- mod_php5_apache2/recipes/php.rb
+++ mod_php5_apache2/recipes/php.rb
@@ -1,3 +1,29 @@
+#Stop Apache
+execute "Restart Apache" do
+  command "sleep 1 && /sbin/service httpd stop && sleep 1"
+end
+
+#Let AWS Opsworks download from Git and deploy to /srv
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+end
+
 # setup Apache virtual host
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'php'
